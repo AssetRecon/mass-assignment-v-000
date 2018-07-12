1198eb76d840e10580de82f4c135315ab9c53dba
Edsel Caprice <caprice@mail.usf.edu>
2018-07-12 00:33:10 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,10 +3,10 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-attributes = {name: "jim", birthday: "today"}
+jim_attributes = {name: "jim", birthday: "today"}
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="),value)}
+    jim_attributes.each {|key, value| self.send(("#{key}="),value)}
     end
   end
 
